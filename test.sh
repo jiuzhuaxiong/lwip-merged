19d9c44547714bf1ed73b27c4b89c9224141e023
Erik Ekman <erik@kryo.se>
2015-10-01 10:12:40 +0200

    Check results of make commands

--- test.sh
+++ test.sh
@@ -3,16 +3,34 @@
 cd contrib/ports/unix/proj/lib
 #build library
 make clean all
+ERR=$?
+if [ $ERR != 0 ]; then
+       echo "library failed"
+       exit 33
+fi
 cd ../minimal
 #build minimal app
 make clean all
+ERR=$?
+if [ $ERR != 0 ]; then
+       echo "minimal failed"
+       exit 33
+fi
 cd ../unixsim
 #build unixsim app
 make clean
 make ARCH=linux all
-make ARCH=linux simrouter
-make ARCH=linux simnode
+ERR=$?
+if [ $ERR != 0 ]; then
+       echo "unixsim failed"
+       exit 33
+fi
 cd ../../check
 #build and run unit tests
 make clean all
+ERR=$?
+if [ $ERR != 0 ]; then
+       echo "unittests build failed"
+       exit 33
+fi
 make check
