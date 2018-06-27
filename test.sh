2a870e97a031dbacf3f12b49914432056f8438ed
Erik Ekman <erik@kryo.se>
2018-06-27 23:38:26 +0200

    Skip building non-existing unix targets

--- test.sh
+++ test.sh
@@ -4,32 +4,7 @@ if [ "$COMPILER" ];
 then
 	export CC=$COMPILER
 fi
-cd contrib/ports/unix/lib
-#build library
-make clean all
-ERR=$?
-if [ $ERR != 0 ]; then
-       echo "library failed"
-       exit 33
-fi
-cd ../minimal
-#build minimal app
-make clean all
-ERR=$?
-if [ $ERR != 0 ]; then
-       echo "minimal failed"
-       exit 33
-fi
-cd ../unixsim
-#build unixsim app
-make clean
-make ARCH=linux all
-ERR=$?
-if [ $ERR != 0 ]; then
-       echo "unixsim failed"
-       exit 33
-fi
-cd ../check
+cd contrib/ports/unix/check
 #build and run unit tests
 make clean all
 ERR=$?
